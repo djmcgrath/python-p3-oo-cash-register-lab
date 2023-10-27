818685082f3fe9c1bcb264e735f0c3982bb85256
Daniel McGrath <djmcgrath95@gmail.com>
2023-10-27 12:23:11 -0600

    completed cash registry

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+
+  def add_item (self, title, price, quantity = 1):
+    self.title = title
+    for i in range(quantity):
+      self.items.append(title)
+    self.price = price
+    self.quantity = quantity 
+    self.total += price * quantity
+    
+  def apply_discount (self):
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      self.total *= (1 - (self.discount/100))
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+
+  def void_last_transaction (self):
+    self.total -= self.price * self.quantity
+
